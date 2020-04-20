6e193fb2c029cc10b0692f1fc6f45827e2242b5e
vagnerPG <vagnergoulart28@gmail.com>
2020-04-20 16:51:31 -0300

    Upload de arquivos master

---
+++ main.py
@@ -0,0 +1,130 @@
+
+import random
+
+from variaveis_letras_numeros_simbolos import (letras_lower,letras_upper,letras_lower_e_upper, pontuacoes,numeros,todos_os_caracteres)
+
+input_usuario_p_tamanho_da_senha = 6
+
+lista_caracteres_importados = [letras_lower,letras_upper,pontuacoes,numeros]
+
+lista_com_os_caracteres = []
+
+guarda_caracteres_diferenciados = []
+
+def mensagem_inicial_pedindo_input():
+  
+  global input_do_usuario_p_gerar_senha
+
+  print("Para sair digite 0")
+  print("Para uma senha mais diferenciada digite 7")
+  input_do_usuario_p_gerar_senha = int(input("Digite 1 para gerar uma senha rápida: "))
+
+def gera_e_imprime_senha_rapida():
+
+  global n_aleatorio
+
+  if input_do_usuario_p_gerar_senha == 1:
+  
+    print()
+
+    for i in range(input_usuario_p_tamanho_da_senha):
+
+      n_aleatorio = random.randrange(len(lista_com_os_caracteres))
+
+      print(lista_com_os_caracteres[n_aleatorio], end = " ")
+    print()
+    print()
+
+def gera_e_imprime_senha_diferenciada():
+
+  if input_do_usuario_p_gerar_senha == 7:
+  
+    while True:
+      
+      pede_inputs_diferenciados_e_soma_os_mesmos()
+
+      if soma_dos_inputs <= input_usuario_p_tamanho_da_senha:
+
+        gera_senha_diferenciada()
+        break
+
+      else:
+        
+        print("\nA quantidade da caracteres excedeu o tamanho selecionada para senha!")
+        print("Você pediu {} caracteres, mas definiu {} como tamanho para senha".format(
+          soma_dos_inputs, input_usuario_p_tamanho_da_senha))
+
+def pede_inputs_diferenciados_e_soma_os_mesmos():
+
+  global input_usuario_p_tamanho_da_senha, input_usuario_quant_n_da_senha, input_usuario_quant_letras_da_senha, input_usuario_quant_pontuacao_da_senha, soma_dos_inputs
+
+  input_usuario_p_tamanho_da_senha = int(input("\nTamanho da senha que você quer: "))
+
+  input_usuario_quant_n_da_senha = int(input("Quantos numeros quer?: "))
+
+  input_usuario_quant_letras_da_senha = int(input("Quantas letras quer?: "))
+
+  input_usuario_quant_pontuacao_da_senha = int(input("Quantas pontuacoes quer?: "))
+
+  # dps que fazer a funcao acima, colocar essa var do lado de fora(se der)
+  soma_dos_inputs = input_usuario_quant_n_da_senha + input_usuario_quant_letras_da_senha + input_usuario_quant_pontuacao_da_senha
+
+def gera_senha_diferenciada():
+  lista_inputs_p_senha_diferenciada = [
+    input_usuario_quant_n_da_senha,
+    input_usuario_quant_letras_da_senha,
+    input_usuario_quant_pontuacao_da_senha]
+  
+  contador_p_iteracao = 0 # isso deve ser relido como '0' msm
+
+  for item in [numeros,letras_lower_e_upper,pontuacoes]:
+
+    for i in range(lista_inputs_p_senha_diferenciada[contador_p_iteracao]):
+
+      guarda_caracteres_diferenciados.append(item[random.randrange(len(item))])
+
+    contador_p_iteracao += 1
+
+  questiona_falta_d_caractere()
+
+  random.shuffle(guarda_caracteres_diferenciados)
+  random.shuffle(guarda_caracteres_diferenciados)
+  
+  print()
+  for caractere in guarda_caracteres_diferenciados: print(caractere, end= " ")
+  print()
+  print()
+
+def questiona_falta_d_caractere():
+
+  if soma_dos_inputs < input_usuario_p_tamanho_da_senha:
+
+    input_faltando_caractere = int(input("\nAinda estah faltando {} caractere(s), quer quer completar com aleatorio(s)? 1 (sim) 2 (nao): ".format(
+      input_usuario_p_tamanho_da_senha - soma_dos_inputs)))
+    print
+
+    if input_faltando_caractere == 1:
+      for i in range(input_usuario_p_tamanho_da_senha - soma_dos_inputs):
+        guarda_caracteres_diferenciados.append(todos_os_caracteres[random.randrange(len(todos_os_caracteres))])
+
+print("Voce está em um gerador de senhas!\n")
+
+while True:
+
+  try:
+
+    mensagem_inicial_pedindo_input()
+
+    if input_do_usuario_p_gerar_senha == 0: break
+
+    for item in lista_caracteres_importados:
+      lista_com_os_caracteres += item
+    
+    gera_e_imprime_senha_rapida()
+
+    gera_e_imprime_senha_diferenciada()
+  
+  except ValueError:
+    print("\nSe atenha aos numeros dos anunciados, Não digite outros valores!\n")
+
+print("\nGerador de senhas fechado.")
---
+++ variaveis_letras_numeros_simbolos.py
@@ -0,0 +1,12 @@
+
+letras_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+letras_upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+letras_lower_e_upper = letras_lower + letras_upper
+
+pontuacoes = ['!', '#', '$', '%', '&', '*', ';', '<', '=', '>', '@', '^', '_']
+
+numeros = [0,1,2,3,4,5,6,7,8,9]
+
+todos_os_caracteres = letras_lower_e_upper + pontuacoes + numeros
